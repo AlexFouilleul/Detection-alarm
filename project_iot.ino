f135c51254c3747757481b271e6a98053869a62f
AlexFouilleul <72225568+AlexFouilleul@users.noreply.github.com>
2022-12-10 18:06:01 +0100

    Add Bluetooth part
    add UUID + test characteristic

--- project_iot.ino
+++ project_iot.ino
@@ -1,62 +1,71 @@
-/*macro definitions of PIR motion sensor pin and LED pin*/
+#include <Adafruit_NeoPixel.h>
+#include <ArduinoBLE.h>
+
+/* definitions of sensors pins */
 #define PIR 12          // PIR Sensor
 #define RED 22          // Red LED
 #define BLUE 24         // Blue LED
 #define GREEN 23        // Green LED
 #define BUZZER 11       // Buzzer
+#define LED_RGB A6      // WS2812 LED
 
-char notes[] = "ccggaagffeeddcc ";
-int beats[] = {1,1,1,1,1,1,2,1,1,1,1,1,1,2,4};
-int tempo = 300;
-
+/* globales variables */
 int cpt = 0;
+int NB_LED = 5;
 
-void play(char note, int duration)
-{
-  char names[] = {'c', 'd', 'e', 'f', 'g', 'a', 'b', 'c'};
-  int freq[] = {1915, 1700, 1519, 1432, 1275, 1136, 1014, 956};
-  for(int i=0; i<8; i++)
-  {
-    if(names[i] == note)
-    {
-      for(long i=0; i<duration*1000L; i+=freq[i]*2)
-      {
-        digitalWrite(BUZZER, HIGH);
-        delay(freq[i]);
-        digitalWrite(BUZZER, LOW);
-        delay(freq[i]);
-      }
-    }
-  }
-}
+/* object and services definition */
+//Adafruit_NeoPixel led_alarm = Adafruit_NeoPixel(NB_LED, LED_RGB, NEO_GRB + NEO_KHZ800);
+BLEService customService("19b10000-e8f2-537e-4f6c-d104768a1214");                           // Service Bluetooth Arduino
+BLEUnsignedIntCharacteristic test("19b10001-e8f2-537e-4f6c-d104768a1214", BLERead | BLENotify);
 
 void runBuzzer()
-{  
-  digitalWrite(RED, LOW);  
-  Serial.println("Moving people detected");
-
-  tone(BUZZER, 440);
-  delay(3000);
+{ 
+  cpt++;
+  digitalWrite(RED, LOW); 
+  for(int rep=0; rep<5; rep++)
+  {
+    for(int freq=1400; freq<=1600; freq++)
+    {
+      tone(BUZZER, freq);
+      delay(5);
+    }
+  }
+  tone(BUZZER, 0);
 } 
 
 void stopBuzzer()
 {
     digitalWrite(RED, HIGH); 
-    //digitalWrite(BUZZER, LOW);
     noTone(BUZZER);
-    Serial.println("Watching");
 }
  
 void setup()
 {
+    // PIR sensor
     pinMode(PIR, INPUT);
+
+    // Serial communication
     Serial.begin(9600); 
     Serial.println("Start alarm");  
 
-    //pinMode(BUZZER, OUTPUT);
-    //noTone(BUZZER);
-    //digitalWrite(BUZZER, LOW);
+    // Buzzer
+    pinMode(BUZZER, OUTPUT);
+    tone(BUZZER, 0);
     
+    // Bluetooth
+    if (!BLE.begin())
+    {
+      Serial.println("Failed.");
+      while(1);
+    }
+    BLE.setLocalName("Alarm");                    // set device name
+    BLE.setAdvertisedService(customService);      // set advertised service UUID
+    customService.addCharacteristic(test);        // add the characteristic to the service
+    BLE.addService(customService);                // add service
+    BLE.setConnectable(true);                     // define possibility to connect to this device
+    BLE.advertise();                              // start advertising
+
+    // LED
     pinMode(RED, OUTPUT);
     pinMode(BLUE, OUTPUT);
     pinMode(GREEN, OUTPUT);
@@ -67,15 +76,14 @@ void setup()
  
 void loop()
 {
-  Serial.println("Moving people detected");
-  if(digitalRead(PIR))          // if it detects the moving people
+  BLEDevice central = BLE.central();      // listen for connection by others devices
+
+  if(digitalRead(PIR))                    // if it detects the moving people
   {
       runBuzzer();
-      cpt++;
   }
   else
   {
       stopBuzzer();
   }
-  //delay(200);
 }
